cbbf33d17937c9f813538889d01fb668017e6e8b
laboneeaktar <labonipub@gmail.com>
2022-11-11 10:28:24 +0600

    sort and search data

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { useRef } from "react";
 import { useState, useEffect } from "react";
 import ServiceCart from "./ServiceCart";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [isAsc, setIsAsc] = useState(true);
+  const [search, setSearch] = useState("");
+  const searchRef = useRef();
+
   useEffect(() => {
-    fetch("https://new-genius-car-server.vercel.app/services")
+    fetch(
+      `http://localhost:5000/services?search=${search}&orders=${
+        isAsc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
         setServices(data);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }, [isAsc, search]);
+
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
 
   return (
     <div>
@@ -24,6 +37,24 @@ const Services = () => {
           humour, <br /> or randomised words which don't look even slightly
           believable.
         </p>
+        <input
+          ref={searchRef}
+          type="text"
+          className="input border border-rose-400"
+        />
+        <button
+          onClick={handleSearch}
+          className="btn btn-error font-normal ml-5 hover:btn-secondary"
+        >
+          Search
+        </button>{" "}
+        <br /> <br />
+        <button
+          onClick={() => setIsAsc(!isAsc)}
+          className="btn btn-outline btn-error font-normal"
+        >
+          Sort {isAsc ? "desc" : "asc"}
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
         {services.map((service) => (
